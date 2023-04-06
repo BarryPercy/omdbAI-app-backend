1bafe3e703bd5f332b68885c37f287522710ca75
Barry Percy <baz.percy@gmail.com>
2023-04-06 01:17:11 +0100

    adding logging for testing

--- src/server.ts
+++ src/server.ts
@@ -6,15 +6,20 @@ import searchRouter from './api/search/index'
 import rateLimit from 'express-rate-limit';
 import 'dotenv/config';
 
+console.log("1")
 const server = Express()
+console.log("2")
 const port = process.env.PORT
 server.use(Express.json())
+console.log("3")
 server.use(cors())
+console.log("4")
 const limiter = rateLimit({
     windowMs: 60 * 1000, // 1 minute
     max: 5
   });
 server.use(limiter);
+console.log("5")
 server.use("/movies", moviesRouter)
 server.use("/search", searchRouter)
 
