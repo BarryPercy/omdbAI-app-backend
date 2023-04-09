cc31c5f858d8fa9caa5e7111d1c7ecccc13d397e
Barry Percy <baz.percy@gmail.com>
2023-04-09 16:59:03 +0100

    Added redis to loading movies

--- src/api/movies/index.ts
+++ src/api/movies/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import axios from "axios";
+import { tedisClient } from "../../redis";
 
 const moviesRouter = express.Router()
 
@@ -14,24 +15,30 @@ interface Movie {
 
 moviesRouter.get("/:imdbId", async (req,res,next)=> { //sends a request to OMDb api with the given imdb id and uses some of the information
     try {                                             //to create a movie object to send to the client
-        const response = await axios.get(`http://www.omdbapi.com/?i=${req.params.imdbId}&apikey=${process.env.API_KEY}`)
-        if(response.status=200){
-            const movie = await response.data as Movie;
-            if(movie.Response==="False"){ //if there is no search we send the movie response which gives the false response.
-                res.send(movie)
-            }else{ //if the response is true then we can send the movie's details.
-                const movieObject = {
-                    title: movie.Title,
-                    poster: movie.Poster,
-                    year: movie.Year,
-                    plot: movie.Plot,
-                    imdbID: movie.imdbID,
-                    Response: movie.Response
-                }
-                res.send(movieObject)
+        let cache = await tedisClient.get(`movie ${req.params.imdbId}`); //added movie to the search so that there's no conflict with search.
+        if(cache === null){ //sends a request top OMDb api if it's not in the cache  
+            const response = await axios.get(`http://www.omdbapi.com/?i=${req.params.imdbId}&apikey=${process.env.API_KEY}`)
+            if(response.status=200){
+                const movie = await response.data as Movie;
+                if(movie.Response==="False"){ //if there is no search we send the movie response which gives the false response.
+                    res.send(movie)
+                }else{ //if the response is true then we can send the movie's details.
+                    const movieObject = {
+                        title: movie.Title,
+                        poster: movie.Poster,
+                        year: movie.Year,
+                        plot: movie.Plot,
+                        imdbID: movie.imdbID,
+                        Response: movie.Response
+                    }
+                    await tedisClient.set(`movie ${req.params.imdbId}`, JSON.stringify(movieObject)); //puts the search results in the cache
+                    res.send(movieObject)
+                }  
             }
-            
+        } else{
+            res.send(JSON.parse(cache.toString()))
         }
+        
     } catch (error) {
         next(error)
     }
--- src/api/search/index.ts
+++ src/api/search/index.ts
@@ -6,12 +6,12 @@ const searchRouter = express.Router()
 searchRouter.get("/:title", async (req,res,next)=> { //gets the search term and gets the top 10 responses
     try {
         try {
-            let cache = await tedisClient.get(req.params.title); //checks if the search is already in the cache
+            let cache = await tedisClient.get(`search ${req.params.title}`); //checks if the search is already in the cache, added search keyword so there's no conflicts with a movie search
             if(cache === null){ //sends a request top OMDb api if it's not in the cache          
                 const response = await axios.get(`http://www.omdbapi.com/?s=${req.params.title}&type=movie&apikey=${process.env.API_KEY}`)
                 if(response.status===200){
                     const search = await response.data; //converts to readable data
-                    await tedisClient.set(req.params.title, JSON.stringify(search)); //puts it in the cache
+                    await tedisClient.set(`search ${req.params.title}`, JSON.stringify(search)); //puts the search results in the cache
                     res.send(search) 
                 }else{
                     res.status(400).send(`Failed to fetch anything with search term ${req.params.title}`)
